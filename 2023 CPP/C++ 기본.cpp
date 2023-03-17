38968f1a2b3a34302ca3133631f28c6f199cd8ca
jangjian <pureunnabi@gmail.com>
2023-03-17 09:15:16 +0900

    C++ style 1

---
+++ 2023 CPP/C++ 기본.cpp
@@ -0,0 +1,9 @@
+﻿// cout 객체 사용
+#include <iostream>
+
+int main(void) {
+	// << 출력연산자
+	std::cout << "Hello world\n";
+
+	return 0;
+}
--- 2023 CPP/문법/C++ 기본.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <stdio.h>
-
-int main(void) {
-	printf("Hello world\n");
-
-	return 0;
-}
