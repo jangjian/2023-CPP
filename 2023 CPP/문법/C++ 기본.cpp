ba15562a6b8980dc7c045a483da6979adeb88673
jangjian <pureunnabi@gmail.com>
2023-03-21 13:49:44 +0900

    포인터

--- 2023 CPP/문법/C++ 기본.cpp
+++ 2023 CPP/문법/C++ 기본.cpp
@@ -1,14 +1,14 @@
 ﻿// cout 객체 사용
-#include <iostream>
+#include <iostream>	// 표준입출력 라이브러리
 
-// 이름 공간
+// 이름 공간 축약
 user namespace std;
 
 int main(void) {
 	int year = 2023;
 
-	// << 출력연산자
-	// endl : 개행
+	// cout << 출력연산자
+	// endl : 개행 객체
 	cout << "Hello world" << year << endl;
 
 	return 0;
