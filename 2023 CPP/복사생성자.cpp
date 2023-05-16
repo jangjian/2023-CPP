a8002036a86964a82d69982dcaab5b3e6a803188
Jian Jang <pureunnabi@gmail.com>
2023-05-16 14:36:26 +0900

    복사생성자

--- 2023 CPP/복사생성자.cpp
+++ 2023 CPP/복사생성자.cpp
@@ -12,6 +12,11 @@ public :
 		cout << "일반생성자 호출" << endl;
 	}
 
+	// 복사생성자(얕은 복사)
+	Munja(const Munja& hj) : str_(hj.str_) {
+		cout << "복사생성자 호출" << endl;
+	}
+
 	~Munja() {
 		delete[] str_;
 		cout << "소멸자 호출" << endl;
@@ -23,7 +28,9 @@ private:
 
 int main(void)
 {
-	Munja m1 = Munja("abc");
+	Munja m1 = Munja("abc");	// 일반 생성자 호출
+	Munja m2 = m1;				
+
 
 	return 0;
 }
