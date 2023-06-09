3fa20d6045b777d7e19eaecb62678e54ab0102c0
Jian Jang <pureunnabi@gmail.com>
2023-06-09 09:17:29 +0900

    상속에 따른 생성/소멸자

--- 2023 CPP/C++ 기본.cpp
+++
@@ -1,9 +0,0 @@
-﻿// cout 객체 사용
-#include <iostream>
-
-int main(void) {
-	// << 출력연산자
-	std::cout << "Hello world\n";
-
-	return 0;
-}
--- 2023 CPP/문법/C++ 기본.cpp
+++ 2023 CPP/문법/C++ 기본.cpp
@@ -1,15 +1,9 @@
 ﻿// cout 객체 사용
-#include <iostream>	// 표준입출력 라이브러리
-
-// 이름 공간 축약
-user namespace std;
+#include <iostream>
 
 int main(void) {
-	int year = 2023;
-
-	// cout << 출력연산자
-	// endl : 개행 객체
-	cout << "Hello world" << year << endl;
+	// << 출력연산자
+	std::cout << "Hello world\n";
 
 	return 0;
 }
