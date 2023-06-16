28863955e6699495c2bcb7b8d3c826842b261a79
Jian Jang <pureunnabi@gmail.com>
2023-06-16 09:58:08 +0900

    순수가상함수, 추상클래스

--- 2023 CPP/상속.cpp
+++ 2023 CPP/상속.cpp
@@ -13,10 +13,12 @@ public:
 
 	// 소멸자에는 무조건 virtual을 넣자(메모리 누수 방지)
 	virtual ~Animal() { cout << "동물 소멸자" << endl; }
-	// 동적 바인딩
-	virtual void Bark(void) { cout << "동물 짖는다" << endl; }
-	virtual void Eat(void) { cout << "동물 먹는다" << endl; }
-	virtual void Hunt(void) { cout << "동물 사냥한다" << endl; }
+
+	// 동적 바인딩(가상함수)
+	// 동물은 추상적인 존재이므로 함수의 구체적인 구현(정의)은 자식클래스에서(순수 가상함수)
+	virtual void Bark(void) = 0;
+	virtual void Eat(void) = 0;
+	virtual void Hunt(void) = 0;
 
 private:
 	int age_;
@@ -39,12 +41,20 @@ public:
 	{
 		cout << "두루두루" << endl;
 	}
+
+	// 순수가상함수(추상메서드)는 반드시 자식에서 구현해야 한다
+	void Eat() override {}
+	void Hunt() override {}
+
 private:
 	int leg_length_;
 };
 
 int main(void)
 {
+	// 추상클래스(하나라도 추상메서드가 존재하는 클래스)는 객체(instance)를 만들 수 없다
+	// Animal* animal = new Animal(1, "1");
+
 	Animal* animal= new Crane(3, "지우", 108);	// 부모의 멤버변수를 사용
 	animal->Bark();	// 두루두루
 	delete animal;
