b10b8460eab66ddfc5ebfc12c953cb7cad19c602
Jian Jang <pureunnabi@gmail.com>
2023-08-01 14:02:47 +0900

    [상속] 기틀을 잡자

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,22 +1,20 @@
+﻿#include <time.h> //time()
+#include <stdlib.h> //srand(), rand()
 ﻿#include <SFML/Graphics.hpp>
-#include <stdlib.h>		// srand(), rand()
-#include <time.h>		// time()
-#include <stdio.h>
 
-#define DIR_UP	0
+#define DIR_UP		0
 #define DIR_DOWN	1
 #define DIR_RIGHT	2
 #define DIR_LEFT	3
-
-#define BODY_MAX	20  //뱀 몸통의 최대길이
+#define BODY_MAX	20
 
 using namespace sf;
 
-const int WIDTH = 1000;							// 픽셀 넓이
-const int HEIGHT = 800;							// 픽셀 높이
-const int BLOCK_SIZE = 50;						// 한 칸이 가지고 있는 픽셀
-const int G_WIDTH = WIDTH / BLOCK_SIZE;			// 그리드의 넓이
-const int G_HEIGHT = HEIGHT / BLOCK_SIZE;		// 그리드의 높이
+const int WIDTH = 1000;						//픽셀 너비
+const int HEIGHT = 800;						//픽셀 높이
+const int BLOCK_SIZE = 50;					//한 칸이 가지고 있는 픽셀
+const int G_WIDTH = WIDTH / BLOCK_SIZE;		//그리드의 너비
+const int G_HEIGHT = HEIGHT / BLOCK_SIZE;	//그리드의 높이
 
 class Object {
 public:
@@ -27,43 +25,46 @@ public:
 
 class Snake {
 public:
-	Snake(int dir, int length) : dir_(dir), length_(length){}
+	Snake(int dir, int length, int score = 0) : dir_(dir), length_(length), score_(score) {}
 
 	int GetDir(void) { return dir_; }
 	int GetLength(void) { return length_; }
-	Object* GetBody() { return body_; }
+	int GetScore() { return score_; }
+	Object* GetBody(void) { return body_; }
 
 	void SetDir(int dir) { dir_ = dir; }
 	void SetLength(int length) { length_ = length; }
-	void IncLength(void) { length_++; }
+	void SetScore(int score) { score_ = score; }
 
-	// body 초기화
-	void InitBody(void)
+	void IncLength(void) { length_++; }
+	void IncScore(int val) { score_ += val; }
+
+	void InitBody()
 	{
+		//body 초기화
 		for (int i = 0; i < BODY_MAX; i++) {
-			body_[i].x_ = -50, body_[0].y_ = -50;		// 뱀의 그리드 좌표
+			body_[i].x_ = -50, body_[i].y_ = -50;		//뱀의 그리드 좌표
+			body_[i].sprite_.setFillColor(Color::Green);
 			body_[i].sprite_.setOutlineColor(Color(0, 128, 0));
 			body_[i].sprite_.setOutlineThickness(5.f);
-			body_[i].sprite_.setFillColor(Color::Green);
 			body_[i].sprite_.setPosition(body_[i].x_ * BLOCK_SIZE, body_[i].y_ * BLOCK_SIZE);
 			body_[i].sprite_.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
 		}
 		body_[0].x_ = 3, body_[0].y_ = 3;
 	}
 
+	//머리 이외의 몸통
 	void UpdateBody(void) {
-
-		// 머리 이외의 몸통
-		for (int i = GetLength() - 1; i > 0; i--) {
+		for (int i = GetLength() - 1; i > 0; i--)
+		{
 			body_[i].y_ = body_[i - 1].y_;
 			body_[i].x_ = body_[i - 1].x_;
 			body_[i].sprite_.setPosition(body_[i].x_ * BLOCK_SIZE, body_[i].y_ * BLOCK_SIZE);
 		}
 	}
 
-	void UpdateHead(void)
-	{
-		// 머리
+	//머리
+	void UpdateHead(void) {
 		if (GetDir() == DIR_UP && body_[0].y_ > 0) {
 			body_[0].y_--;
 		}
@@ -79,9 +80,10 @@ public:
 		body_[0].sprite_.setPosition(body_[0].x_ * BLOCK_SIZE, body_[0].y_ * BLOCK_SIZE);
 	}
 
-private :
+private:
 	int dir_;
 	int length_;
+	int score_;
 	Object body_[BODY_MAX];
 };
 
@@ -92,28 +94,35 @@ public:
 	RectangleShape sprite_;
 };
 
-int main(void)
-{
-	printf("%d", DIR_RIGHT);
-	
-
+int main() {
 	srand(time(NULL));
+	RenderWindow window(VideoMode(WIDTH, HEIGHT), "SnakeGame"); //창만들기
+	//1초 동안 처리하는 횟수를 60으로 제한
+	//Frame Per Second를 60으로 조절
+	window.setFramerateLimit(15);
 
-	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Snake Game");
-	// 컴퓨터가 1초 동안 처리하는 횟수를 60으로 제한한다. 
-	// Frame Per Second를 60으로 조절
-	window.setFramerateLimit(20);
+	//폰트 설정
+	Font font;
+	if (!font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf")) {
+		printf("폰트 불러오기 실패");
+		return -1;
+	}
+
+	Text t_info;
+	t_info.setFont(font);  //글씨폰트지정
+	t_info.setFillColor(Color::Magenta);  //글씨색
+	t_info.setCharacterSize(50);  //글씨크기
+	t_info.setPosition(0, 0);  //글씨위치
+
+	char t_info_buf[100];
 
 	Snake snake = Snake(DIR_DOWN, 1);
 	snake.InitBody();
 
-	
-
 	Apple apple;
 	apple.x_ = rand() % G_WIDTH, apple.y_ = rand() % G_HEIGHT;
 	apple.sprite_.setFillColor(Color::Red);
-	// 50을 뺀 이유는 사과가 화면 밖에 벗어나지 않게 하기 위함
-	apple.sprite_.setPosition(apple.x_ *BLOCK_SIZE, apple.y_ *BLOCK_SIZE);
+	apple.sprite_.setPosition(apple.x_ * BLOCK_SIZE, apple.y_ * BLOCK_SIZE);
 	apple.sprite_.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
 
 
@@ -121,52 +130,56 @@ int main(void)
 		Event e;
 		while (window.pollEvent(e))
 		{
-			// 윈도우의 x를 눌렀을 때 창이 닫아지도록
-			// Frame Per Second를 60으로 조절
+			//input
+			//윈도우의 x를 눌렀을 때 창이 닫아지도록 
 			if (e.type == Event::Closed)
 				window.close();
 		}
 
-		// 방향키가 동시에 눌러지지 않도록 else 처리
+		//방향키가 동시에 눌러지지 않도록 else처리
 		if (Keyboard::isKeyPressed(Keyboard::Right)) {
 			snake.SetDir(DIR_RIGHT);
-
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Left)) {
 			snake.SetDir(DIR_LEFT);
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Up)) {
 			snake.SetDir(DIR_UP);
-
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
 			snake.SetDir(DIR_DOWN);
-
 		}
-		// update
+
+		//update
+		sprintf(t_info_buf, "score : %d\n", snake.GetScore()); //t_info_buf안에 뒤에 출력할 문장이 들어감
+		t_info.setString(t_info_buf);  //글씨가 계속 갱신되도록
+
 		snake.UpdateBody();
 		snake.UpdateHead();
 
-		// TODO : GetBody() 멤버접근 방법 바꿔보기
-		// 뱀이 사과를 먹었을 때
-		if (snake.GetBody()[0].x_ == apple.x_ && snake.GetBody()[0].y_ == apple.y_)
-		{
-			// 사과 위치전환
+		//뱀이 사과를 먹었을 때, 
+		if (snake.GetBody()[0].x_ == apple.x_ && snake.GetBody()[0].y_ == apple.y_) {
+			//사과 위치전환
 			apple.x_ = rand() % G_WIDTH, apple.y_ = rand() % G_HEIGHT;
 			apple.sprite_.setPosition(apple.x_ * BLOCK_SIZE, apple.y_ * BLOCK_SIZE);
 
-			// 뱀의 길이를 변화
-			if(snake.GetLength()<20)
+			//점수 증가
+			snake.IncScore(5);
+
+			//뱀의 길이변화
+			if (snake.GetLength() < 20) {
 				snake.IncLength();
+			}
 		}
 
-		// render
+		//render
 		window.clear();
-		
-		for (int i = 0; i < snake.GetLength(); i++)
-			window.draw(snake.GetBody()[i].sprite_);
-		window.draw(apple.sprite_);		// draw를 늦게 할 수록 더 위에 있다.
 
+		for (int i = 0; i < snake.GetLength(); i++) {
+			window.draw(snake.GetBody()[i].sprite_);
+		}
+		window.draw(apple.sprite_);  //draw늦게할수록 더 위에 있다. 
+		window.draw(t_info);
 		window.display();
 	}
 
